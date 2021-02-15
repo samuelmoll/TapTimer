290bf81c01bee8ba3771190f27832c755dddf2e1
samuelmoll <samuel.moll@uqconnect.edu.au>
2021-02-15 11:23:09 +1000

    Clean up menu operation

--- lib/States/States.cpp
+++ lib/States/States.cpp
@@ -16,33 +16,33 @@ void InitScheduleState::update(StateMachine& machine){
 }
 
 void RunScheduleState::update(StateMachine& machine){
-    Serial.println("Running Schedule:");
-    char keypressed = NO_KEY;
-    keypressed = machine.getDisplayMenu()->wait_for_keypad(0);
-    while(keypressed == NO_KEY) {
-        machine.getValveController()->runSchedule();
-    }
-  
+  Serial.println("Running Schedule:");
+  char keypressed = NO_KEY;
+  while(keypressed == NO_KEY) {
+      machine.getValveController()->runSchedule();
+      keypressed = machine.getDisplayMenu()->getKeypress();
+  }
   if(machine.transitionsEnabled()){setState(machine, new MainMenuState());}
 }
 
 
 void MainMenuState::update(StateMachine& machine){
-    Serial.println("Main Menu State:");
-    char nextState;
-    nextState = machine.getDisplayMenu()->mainMenu();
-    if(nextState == 1) {
-      machine.getEEPROMController()->updateEEPROM();
-      if(machine.transitionsEnabled()) {setState(machine, new RunScheduleState());}
-    } else if(nextState == 2) {
-      if(machine.transitionsEnabled()) {setState(machine, new RunOnceState());}
-    }
+  Serial.println("Main Menu State:");
+  char nextState;
+  nextState = machine.getDisplayMenu()->mainMenu();
+  if(nextState == 1) {
+    machine.getEEPROMController()->updateEEPROM();
+    if(machine.transitionsEnabled()) {setState(machine, new RunScheduleState());}
+  } else if(nextState == 2) {
+    if(machine.transitionsEnabled()) {setState(machine, new RunOnceState());}
+  }
 }
 
 void RunOnceState::update(StateMachine& machine){
   Serial.println("Run Once State:");
   int valveSelect = machine.getDisplayMenu()->valveSelectRunOnce();
   if(valveSelect <= 0) {
+    machine.getDisplayMenu()->setLCDOff();
     if(machine.transitionsEnabled()) {setState(machine, new RunScheduleState());}
   } else {
     int last_valve = machine.getValveController()->runScheduleOnceInit(valveSelect);
